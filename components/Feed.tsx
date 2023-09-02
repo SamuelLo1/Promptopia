670ece8036f9351e0f1e65100a61fcf159ac3a90
SamuelLo <samuello300@gmail.com>
2023-09-01 22:26:23 -0700

    created route in prompt folder to use GET request

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -3,23 +3,24 @@ import { useState, useEffect } from "react"
 import PromptCard from "./PromptCard";
 
 //is used to store the prompts
-const PromptCardList = ({ data, handleTagClick}) => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className ="mt-16 prompt_layout">
-      {data.map((post) =>(
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
         <PromptCard
-          key = {post._id}
-          post = {post}
-          handleTagClick = {handleTagClick}
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
         />
       ))}
     </div>
-  )
-}
+  );
+};
 //display prompts in feed
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
   const [posts, setPosts] = useState([])
+  
   const handleSearchChange = (e) => {
 
   }
@@ -31,6 +32,9 @@ const Feed = () => {
 
       setPosts(data);
     }
+
+    console.log(posts)
+    fetchPosts();
   }, []);
   return (
     <section className="feed">

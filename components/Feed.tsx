4fb592f081cab380757ae52bf713c5930df62a9b
SamuelLo <samuello300@gmail.com>
2023-09-01 21:59:43 -0700

    setup feed page

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,55 @@
-import React from 'react'
+'use client'
+import { useState, useEffect } from "react"
+import PromptCard from "./PromptCard";
 
-const Feed = () => {
+//is used to store the prompts
+const PromptCardList = ({ data, handleTagClick}) => {
   return (
-    <div>Feed</div>
+    <div className ="mt-16 prompt_layout">
+      {data.map((post) =>(
+        <PromptCard
+          key = {post._id}
+          post = {post}
+          handleTagClick = {handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+//display prompts in feed
+const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([])
+  const handleSearchChange = (e) => {
+
+  }
+  //fetch data
+  useEffect(() =>{
+    const fetchPosts = async ()=> {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data);
+    }
+  }, []);
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList
+        data={posts}
+        handleTagClick={()=> {}}
+      />
+    </section>
   )
 }
 

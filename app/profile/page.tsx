82d5f0bb96250636609879af9c281dbc540c0ae9
SamuelLo <samuello300@gmail.com>
2023-09-04 00:04:31 -0700

    Creating new profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Profile from "@components/Profile";
+
+const myProfile = () => {
+  const {data : session } = useSession();
+  const [posts, setPosts] = useState([]);
+  useEffect(() =>{
+    const fetchPosts = async ()=> {
+      const response = await fetch('/api/users/${session?.user.id}/posts');
+      const data = await response.json();
+
+      setPosts(data);
+    }
+
+    console.log(posts);
+    if(session?.user.id){
+      fetchPosts();
+    }
+    fetchPosts();
+  }, []);
+
+  const handleEdit = ()=>{
+
+  }
+
+  const handleDelete = async () => {
+
+  }
+
+  return (
+    <Profile
+      name="My"
+      desc = "Welcome to your personalized profile page"
+      data ={[posts]}
+      handleEdit={}
+      handleDelete={}
+    />
+
+  )
+}
+
+export default myProfile
